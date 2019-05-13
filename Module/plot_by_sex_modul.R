5e6a036d608a9d2c1da883424dbf0a5dbfe9e2f7
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-05-13 11:54:51 +0200

    Added the possibility to plot by a certain metadata

--- Module/plot_by_sex_modul.R
+++ Module/plot_by_sex_modul.R
@@ -11,13 +11,52 @@ source(
 
 plot_by_sexUI <- function(id, dictionary_id) {
   ns <- NS(id)
-  tagList(
-    checkboxGroupInput(ns("Group"),label = "Group by metadata",choices = c("Gender")),
-  
-  actionButton(ns("refresh_Plot"), label = "Update Plot")
-)
-  }
+  tagList(box(
+    textInput(
+      ns("word"),
+      placeholder = "lower cases",
+      label = "word",
+      value = "Beispiel",
+      width = 600
+    ),
+    selectInput(
+      ns("intervall"),
+      label = "Intervall",
+      choices = list(month = "month", year = "year")
+    ),
+    actionButton(ns("refresh_plot"), label = "Update Plot"),
+    radioButtons(
+      ns("radio_button"),
+      label = "Select your Metadata",
+      choiceNames = c("none", "Sex"),
+      choiceValues = c(0, 1)
+    ),
+    box(plotlyOutput(ns("bar_chart_word")), width = 50)
+  ))
+}
 
 plot_by_sex <- function(input, output, session) {
-  
+  word_plot <- eventReactive(input$refresh_plot, {
+    if (input$radio_button == 0) {
+      pa <- plotte_word_sex(
+        word = input$word,
+        dfm_input = dfm_data(),
+        intervall = input$intervall,
+        group_by_sex = FALSE
+      )
+      return(pa)
+    }
+    else if (input$radio_button == 1) {
+      pa <- plotte_word_sex(
+        word = input$word,
+        dfm_input = dfm_data(),
+        intervall = input$intervall,
+        group_by_sex = TRUE
+      )
+      return(pa)
+    }
+  })
+  output$bar_chart_word <- renderPlotly({
+    word_plot()
+  })
 }
