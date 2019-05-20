e26ef113d5c4f759eae404dbe29f64707abe943b
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-05-20 11:45:30 +0200

    Modul und Funktion funktionieren nun so wie sie sollen, als nächste muss die Unabhängigkeit vom Token_data Objekt hergestellt werden

--- Module/collocations_modul.R
+++ Module/collocations_modul.R
@@ -21,29 +21,29 @@ find_collocationUI <- function(id) {
       step = 1,
       width = 100
     ),
-    textInput(
+    sliderInput(
       ns("min_count"),
-      placeholder = "lower cases",
-      label = "Minimum Count of the Collocation",
-      value = "10,20,30",
-      width = 300
+      label = "Minimum count of the Collocation",
+      min = 10,
+      max = 300,
+      value = 30,
+      step = 5
+      
     ),
-  dataTableOutput(ns("coll_table"),width = 300)  
+    dataTableOutput(ns("coll_table"), width = 900)
     
   )
 }
 
 find_collocation <- function(input, output, session) {
   col_datatable <- eventReactive(input$get_coll, {
-    if (typeof(input$min_count) == "double") {
-      col_dataframe <- find_coll(
-        Token_object = Token_data,
-        size = input$size,
-        min_count = input$min_count
-      )
-    col_datatable <- setDT(col_dataframe)
-    return(col_datatable)
-      }})
-  output$coll_table <- renderDataTable(col_datatable)
-    
-  }
+    col_dataframe <- find_coll(
+      Token_object = Token_data,
+      size = input$size,
+      min_count = input$min_count
+    )
+    return(col_dataframe)
+  })
+  output$coll_table <- renderDataTable(col_datatable())
+  
+}
