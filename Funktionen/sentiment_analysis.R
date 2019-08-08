3e2b7f8a2f8b1d75091c538c66011bb29afb9a58
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-08-08 16:35:09 +0200

    nur Code etwas verbessert und die Funktion umbenannt

--- Funktionen/sentiment_analysis.R
+++ Funktionen/sentiment_analysis.R
@@ -1,30 +1,13 @@
-lengths(data_dictionary_LSD2015)
-Token_data_new <-
-  tokens_lookup(Token_data, dictionary = data_dictionary_LSD2015[1:4])
-head(Token_data_new, 10)
-head(Token_data, 2)
-data_dictionary_LSD2015[1]
 
-
-Token_Test <- Token_data[1:30]
-Token_Test
-
-data_good_bad <-
-  tokens_lookup(Token_Test, data_dictionary_LSD2015[1:2])
-data_good_bad
-dfm_sent <- dfm(data_good_bad)
-head(dfm_sent, 20)
-docvars(dfm_sent)
 
 sentiment_analysis <-
   function(Token_object,
            dictionary = data_dictionary_LSD2015,
            intervall = "year") {
-    #month--------
+    #year------------------
     if ("year" == intervall) {
-      Token_sent <-
-        tokens_lookup(Token_object, dictionary = dictionary)
-      dfm_sent <- dfm(Token_sent)
+      dfm_sent <-
+        dfm(tokens_lookup(Token_object, dictionary = dictionary))
       
       dfm_runtime <- dfm_group(dfm_sent, groups = "year")
       data_runtime <-
@@ -40,25 +23,27 @@ sentiment_analysis <-
           x = data_runtime[, 3],
           type = "bar",
           name = "Positive"
-        )%>%
+        ) %>%
           add_trace(y = data_runtime[, 2], name = "Negative") %>%
           layout(yaxis = list(title = 'Count'), barmode = 'group')
         
       )
     }
+    #Else keine Daten vorhanden gebe Nullen aus----------------
     else
       data_runtime <-
         data.frame(word_count = as.vector(rep(0, nrow(dfm_runtime))),
                    Zeit = docvars(dfm_runtime, 'month'))
-      return(
-        plot_ly(
-          data_runtime,
-          y = data_runtime[, 1],
-          x = data_runtime[, 2],
-          type = "bar",
-          name = "char_title"
-        )
+    return(
+      plot_ly(
+        data_runtime,
+        y = data_runtime[, 1],
+        x = data_runtime[, 2],
+        type = "bar",
+        name = "char_title"
       )
+    )
     
   }
-sentiment_analysis(Token_object = Token_data)
+#Test-------------
+#sentiment_analysis(Token_object = Token_data)
