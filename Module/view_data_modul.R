174b901519f6287cc7f8ba902afcfdf8fb83f1ce
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-08-23 11:07:36 +0200

    Code Cleaning

--- Module/view_data_modul.R
+++ Module/view_data_modul.R
@@ -1,28 +1,21 @@
 
-
 view_dataUI <- function(id) {
   ns <- NS(id)
-  tagList(
-  actionButton(ns("update_table"), label = "Update Table"),
-  DTOutput(ns("text_data"))
-  )
+  tagList(actionButton(ns("update_table"), label = "Update Table"),
+          DTOutput(ns("text_data")))
 }
 
 #Server--------
 view_data <- function(input, output, session) {
   table_text <- eventReactive(input$update_table, {
-    table_data <- datatable(
-      corpus_data()$documents[, c(1,3, 4,5,6,8,9)])
-      return(table_data)
+    table_data <- datatable(corpus_data()$documents[, c(1, 3, 4, 5, 6, 8, 9)])
+    return(table_data)
   })
   
   output$text_data <-
     DT::renderDataTable(table_text(),
-      options = list(autoWidth = TRUE,
-                     columnDefs = list(
-                       list(width = "600px", targets = table_text()$texts)
-                     )))
+                        options = list(autoWidth = TRUE,
+                                       columnDefs = list(
+                                         list(width = "600px", targets = table_text()$texts)
+                                       )))
 }
-
-
-    
