689c120719b5b50be712152e85c72aa971c9ee82
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-09-17 09:00:48 +0200

    Minimale Anpassung

--- Funktionen/data_prep_dfm.R
+++ Funktionen/data_prep_dfm.R
@@ -1,5 +1,5 @@
 data_prep_dfm <-
-  function(pathway = paste0(getwd(), "/Daten/Amazon_data/Musical_Instruments_5.json")) {
+  function(pathway = paste0(getwd(), "/Daten/Text_data/Musical_Instruments_5.json")) {
     data_amazon <- stream_in(pathway)
     
     #Umwandlung der Zeit in Jahr und dann dem Data Frame hinzugefuegt-------------------
@@ -33,12 +33,12 @@ data_prep_dfm <-
 
 # #erste Initialisierung für corpus_data-----------------------
 # data_amazon <- stream_in(paste0(getwd(), "/Daten/Amazon_data/Musical_Instruments_5.json"))
-#
+# 
 # #Umwandlung der Zeit in Jahr und dann dem Data Frame hinzugefuegt
 # data_amazon$year <- year(anytime(data_amazon$unixReviewTime))
 # data_amazon$month <- month(anytime(data_amazon$unixReviewTime))
 # data_amazon$doc_id <- c(1:nrow(data_amazon))
-#
+# 
 # #Aufbereitung
 # corpus_data <-
 #   corpus(data_amazon, docid_field = "doc_id", text_field = "reviewText")
