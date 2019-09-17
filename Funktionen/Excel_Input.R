3f41b05f4b7485def75299251d913179018bbada
Knowerbescheidt <janolaferiksen@yahoo.de>
2019-09-17 09:00:27 +0200

    Lösung des Problems mit Dictionaries in denen nur eine Zeile steht vorher wurde die erste Zeile als ColnName genommen somit wurde die erste Zeile ignoriert

--- Funktionen/Excel_Input.R
+++ Funktionen/Excel_Input.R
@@ -7,7 +7,9 @@ import_excel <- function(filename) {
              "/Daten/Dictionaries/",
              filename),
       sheet = 1,
-      cols = 1
+      cols = 1,colNames = FALSE, 
+      skipEmptyRows = TRUE,
+      rowNames = FALSE
     )
   print(typeof(data_excel))
   print(data_excel)
@@ -28,8 +30,12 @@ import_excels <- function(liste) {
                "/Daten/Dictionaries/",
                file = name),
         sheet = 1,
-        cols = 1
-      )
+        cols = 1,
+        colNames = FALSE,
+        skipEmptyRows = TRUE,
+        rowNames = FALSE,
+        startRow = 1
+        )
     list_excel <- list(data_excel[, 1])
     names(list_excel) <- name
     dictio_master[[i]] <- dictionary(list_excel)
@@ -38,4 +44,3 @@ import_excels <- function(liste) {
   }
   return(dictio_master)
 }
-
